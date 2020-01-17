c2eeccaf295451207506b555c9a057c272dc0008
Maksim <Lukianchukma@gmail.com>
2020-01-18 02:07:16 +0300

    Добавил подключение к БД

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const express = require('express')
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
 
-app.listen(5000, () => console.log(`App has been started`))
+const PORT = config.get('port') || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+  } catch(e) {
+    console.log('Server Error:', e.message);
+    process.exit(1)
+  }
+}
+
+start();
